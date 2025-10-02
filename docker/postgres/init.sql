463139bb4a7fbff998877bc8ec94d8118811b745
veidz <joaovictorveidz@gmail.com>
2025-10-02 20:09:58 -0300

    chore: adjust compose and init sql

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -5,12 +5,3 @@ CREATE DATABASE notifications_service;
 GRANT ALL PRIVILEGES ON DATABASE auth_service TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE tasks_service TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE notifications_service TO postgres;
-
-\c auth_service;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-\c tasks_service;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-\c notifications_service;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
